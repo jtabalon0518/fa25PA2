d224900dd11b634f10499295ad123e7e7a5ae8a6
jtabalon0518 <jtabalon0518@sdsu.edu>
2025-11-03 16:25:34 -0800

    Comments added

--- main.cpp
+++ main.cpp
@@ -34,9 +34,9 @@ int main() {
     int nextFree = createLeafNodes(freq);
 
     // Tracking, remove later
-    for (int i = 0; i < 9; i++) {
-        cout << leftArr[i] << " " << rightArr[i] << " " << charArr[i] << " " << weightArr[i] << endl;
-    }
+    // for (int i = 0; i < 9; i++) {
+    //     cout << leftArr[i] << " " << rightArr[i] << " " << charArr[i] << " " << weightArr[i] << endl;
+    // }
 
     // Step 3: Build encoding tree using your heap
     int root = buildEncodingTree(nextFree);
@@ -112,17 +112,19 @@ int buildEncodingTree(int nextFree) {
     }
 
     while (heap.size > 1) {
+        // Gets index stored in next two smallest nodes assigning them to left/right
         int left = heap.pop(weightArr);
         int right = heap.pop(weightArr);
 
+        // Adds the weights of the two nodes, combining them into one and assigns the left to leftArr and right to rightArr
         weightArr[nextFree] = weightArr[left] + weightArr[right];
         leftArr[nextFree] = left;
         rightArr[nextFree] = right;
 
         // Tracing, remove later
-        cout << nextFree << " " << weightArr[nextFree] << " Left: " << leftArr[nextFree] << " Right: " << rightArr[nextFree] << endl;
+        // cout << nextFree << " " << weightArr[nextFree] << " Left: " << leftArr[nextFree] << " Right: " << rightArr[nextFree] << endl;
 
-
+        // Pushes the parent and moves onto next free index
         heap.push(nextFree, weightArr);
         nextFree++;
     }
@@ -139,6 +141,7 @@ void generateCodes(int root, string codes[]) {
 
     stack<pair<int, string>> stack;
     string curr_code;
+    // Pushing our root index (from buildEncodingTree) and an empty string for our code
     stack.push({root, curr_code});
 
     int i = 0;
@@ -149,9 +152,11 @@ void generateCodes(int root, string codes[]) {
 
         cout << curr_node << ": left =" << leftArr[curr_node] << " right =" << rightArr[curr_node] << " char =" << charArr[curr_node] << endl;
 
+        // If leftArr and rightArr contain -1, it is a leaf node so we record the code in codes
         if (leftArr[curr_node] == -1 && rightArr[curr_node] == -1) {
             codes[charArr[curr_node] - 'a'] = curr_code;
         } else {
+            // Pushes the left node and then the right node into stack, the right will be processed first when we come back around to pop (stack is LIFO)
             stack.push({leftArr[curr_node],curr_code + "0"});
             stack.push({rightArr[curr_node],curr_code + "1"});
         }
